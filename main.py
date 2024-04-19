863a14301857ccd02dd635ab93258e6d6a560171
AleksandraX <xodyrevaaleksandra@gmail.com>
2024-04-19 16:41:09 +0300

    not bad commit)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index() -> str:
-    return '<p>Hello Flask!</p>'
+    return render_template("base.html")
+
+
+@app.route('/yes_yes_yes')
+def fn_yes() -> str:
+    return render_template("yes.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
